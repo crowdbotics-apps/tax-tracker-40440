9fe056db9a98e1d889c409b858414ee0b5bb3c8f
Phil Newman <phil.newman@gmail.com>
2023-05-03 22:19:10 -0700

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "tax-tracker-40440",
-  url: "https://tax-tracker-40440.botics.co",
-  api: "https://tax-tracker-40440.botics.co/api/v1"
-}
+  "name": "tax-tracker-40440",
+  "url": "https://tax-tracker-40440.botics.co",
+  "api": "https://tax-tracker-40440.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
