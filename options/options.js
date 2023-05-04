48741abc9e1c1b714ea78277c59d1b0d7a5aa799
Crowdbotics <team@crowdbotics.com>
2023-05-04 05:02:14 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tax-tracker-40440",
+  url: "https://tax-tracker-40440.botics.co",
+  api: "https://tax-tracker-40440.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
